da9308f66e14789b3331012d244acb9ac3cd370c
Nguyễn Hoàng Sơn <sonnguyen9616@gmail.com>
2024-11-27 08:43:47 +0700

    init base project

---
+++ src/main/java/com/sonnguyen/iamservice2/IamService2Application.java
@@ -0,0 +1,13 @@
+package com.sonnguyen.iamservice2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IamService2Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(IamService2Application.class, args);
+    }
+
+}
