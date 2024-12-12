73918884af615a86223e0780791c1145074e6fd6
Nguyễn Hoàng Sơn <sonnguyen9616@gmail.com>
2024-12-12 16:11:39 +0700

    Validating all row before saving user's data Returning entire data violations in excel file

--- src/main/java/com/sonnguyen/iamservice2/exception/WorkbookValidationException.java
+++ src/main/java/com/sonnguyen/iamservice2/exception/WorkbookValidationException.java
@@ -1,19 +1,15 @@
 package com.sonnguyen.iamservice2.exception;
 
+import com.sonnguyen.iamservice2.viewmodel.WorkbookValidationMessage;
 import lombok.Getter;
 
 import java.util.List;
 
 @Getter
 public class WorkbookValidationException extends RuntimeException{
-    List<String> violations;
-    public WorkbookValidationException(String message, List<String> violations) {
-        super(message);
-        this.violations=violations;
+    List<WorkbookValidationMessage> messages;
+    public WorkbookValidationException(List<WorkbookValidationMessage> messages) {
+        super();
+        this.messages=messages;
     }
-
-    public static  WorkbookValidationException create(int rowIndex,int colIndex, List<String> violations) {
-        return new WorkbookValidationException(String.format("Failure Validation in {row:%s, col::%s} ",rowIndex,colIndex),violations);
-    }
-
 }
